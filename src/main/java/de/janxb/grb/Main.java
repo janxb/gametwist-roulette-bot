f430dfe567624788db0855cc4cc1aed9c06627e4
janxb <github@janbrodda.de>
2017-07-10 22:53:23 +0200

    development, first working version

--- src/main/java/de/janxb/grb/Main.java
+++ src/main/java/de/janxb/grb/Main.java
@@ -6,32 +6,54 @@ import org.sikuli.script.*;
 public class Main {
     public static void main(String[] args) throws FindFailed, InterruptedException {
         Screen s = new Screen();
+        s.setAutoWaitTimeout(20);
+
+        /*
         App app = App.open("google-chrome --incognito");
         app.waitForWindow();
         s.type("gametwist.com");
         s.type(Key.ENTER);
 
-        s.setAutoWaitTimeout(20);
-
-        s.wait("src/main/resources/gametwist_nickname.png").click();
+        s.wait("src/main/resources/website_input_nickname.png").click();
         s.type("USERNAME");
-        s.wait("src/main/resources/gametwist_password.png").click();
+        s.wait("src/main/resources/website_input_password.png").click();
         s.type("PASSWORD");
         s.type(Key.ENTER);
 
-        s.wait("src/main/resources/gametwist_casino.png").click();
+        s.wait("src/main/resources/website_button_casino.png").click();
 
-        s.wait("src/main/resources/gametwist_roulette.png").click();
+        s.wait("src/main/resources/website_button_roulette.png").click();
 
-        s.wait("src/main/resources/roulette_table.png").click();
+        s.wait("src/main/resources/app_button_table.png").click();
 
-        s.waitVanish("src/main/resources/text_loading.png");
+        s.waitVanish("src/main/resources/app_text_loadmoney.png");
+        */
 
-        for (int i = 0; i <= 3; i++) {
-            s.wait("src/main/resources/table_black.png").click();
-            s.click("src/main/resources/button_quickspin.png");
-            Match m = s.exists("src/main/resources/result_black.png");
-            System.out.println(m);
+        int lostCount = 0;
+        int maxLostCount = 3;
+
+        //noinspection InfiniteLoopStatement
+        while (true) { // main application loop
+            Match blackButton = s.wait("src/main/resources/app_table_black.png");
+            for (int clickCount = 0; clickCount < (Math.pow(2, lostCount)); clickCount++) { // loop for placing the bets
+                blackButton.click();
+            }
+            s.wait("src/main/resources/app_button_quickspin.png").click();
+            Thread.sleep(2000);
+
+            boolean isGameWin = (null == s.exists(new Pattern("src/main/resources/app_text_gamelost.png").exact(), 0.1));
+            if (isGameWin) {
+                lostCount = 0;
+                System.out.println("Game won, resetting lostCount");
+            } else {
+                lostCount++;
+                System.out.println("Game lost, incrementing lostCount");
+                if (lostCount >= maxLostCount) {
+                    lostCount = 0;
+                    System.out.println("maxLostCount overstepped, resetting lostCount");
+
+                }
+            }
         }
     }
 }
